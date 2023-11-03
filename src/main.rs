c44d4364a4ecfb469395b2f972c31d4858364c7c
Nick Hughes <689543+nickjhughes@users.noreply.github.com>
2023-11-04 10:58:49 +1100

    Pass through stdout and stderr

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,10 @@ fn main() -> Result<()> {
             )
         })?;
 
-    if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout)?;
-        println!("{}", std_out);
-    } else {
-        std::process::exit(1);
-    }
+    let std_out = std::str::from_utf8(&output.stdout)?;
+    let std_err = std::str::from_utf8(&output.stderr)?;
+    print!("{}", std_out);
+    eprint!("{}", std_err);
 
     Ok(())
 }
