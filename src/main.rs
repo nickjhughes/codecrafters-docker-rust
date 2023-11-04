9ee311ba887810800e49615dccfce099ab63a80f
Nick Hughes <689543+nickjhughes@users.noreply.github.com>
2023-11-04 11:27:42 +1100

    Use sandbox

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
 use anyhow::{Context, Result};
+use std::path::PathBuf;
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
-    let output = std::process::Command::new(command)
+
+    // Create temporary directory
+    let temp_dir = tempfile::tempdir().context("failed to create temp directory")?;
+
+    // Create /dev/null in temporary directory
+    std::fs::create_dir_all(temp_dir.path().join("dev/null"))
+        .context("failed to create /dev/null")?;
+
+    // Copy command binary into the temporary directory
+    let command_path = PathBuf::from(command);
+    let command_binary = command_path.file_name().unwrap();
+    let temp_command_path = temp_dir.path().join(command_binary);
+
+    std::fs::copy(&command_path, &temp_command_path).context(format!(
+        "failed to copy command binary from {:?} to {:?}",
+        &command_path, &temp_command_path,
+    ))?;
+
+    // chroot and chdir into the temporary directory
+    std::os::unix::fs::chroot(&temp_dir).context("failed to chroot")?;
+    std::env::set_current_dir("/").context("failed to chdir")?;
+
+    let new_command_binary = PathBuf::from("/").join(command_binary);
+    let output = std::process::Command::new(&new_command_binary)
         .args(command_args)
         .output()
         .with_context(|| {
             format!(
-                "Tried to run '{}' with arguments {:?}",
-                command, command_args
+                "Tried to run {:?} with arguments {:?}",
+                new_command_binary, command_args
             )
         })?;
 
